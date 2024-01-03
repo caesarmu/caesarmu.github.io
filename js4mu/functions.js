72c8e99aba2cfe0d327065dbd16d04211d400000
Mu <caesarmu@users.noreply.github.com>
2024-01-03 09:36:23 +0800

    Add files via upload

---
+++ js4mu/functions.js
@@ -0,0 +1,27 @@
+function resizeIframe(obj) {
+    obj.style.height = (obj.contentWindow.document.body.scrollHeight+5) + 'px';
+}
+
+function GetQueryString(name) {  
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");  
+    var r = window.location.search.substr(1).match(reg);  //获取url中"?"符后的字符串并正则匹配
+    var context = "";  
+    if (r != null)  
+         context = r[2];  
+    reg = null;  
+    r = null;  
+    return context == null || context == "" || context == "undefined" ? "" : context;  
+}
+
+function dataURLtoFile(dataurl, filename) {
+        var arr = dataurl.split(','),
+            mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[1]),
+            n = bstr.length,
+            u8arr = new Uint8Array(n);
+        while(n--){
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+return new File([u8arr], filename, {type:mime});
+}
+
